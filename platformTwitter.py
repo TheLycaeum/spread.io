d23d0730c1838e2054d623c3fb01ecba68277fbd
athul <kpathul@outlook.com>
2019-03-31 23:09:47 +0530

    Adds functions into class Twitter

--- platformTwitter.py
+++ platformTwitter.py
@@ -1,48 +1,39 @@
-import tweepy
 import webbrowser
 import time
+import tweepy
+from platform import Platform
 
+class Twitter(Platform):
+    "Platform for twitter"
 
-consumer_key='xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-consumer_secret='xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-        
-def get_url():
-    # get access token from the user and redirect to auth URL
-    auth_url = auth.get_authorization_url()
-    return auth_url
-    
-def open_browser(url):
-    """open the url on browser"""
-    webbrowser.open(url)
-    time.sleep(1)
+    def load_app_apikey(self):
+        "Loads app api key"
+        consumer_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+        consumer_secret = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+        self.access = tweepy.OAuthHandler(consumer_key, consumer_secret)
 
-def get_key_and_secret():
-    # ask user to verify the PIN generated in broswer
-    verifier = input('PIN: ').strip()
-    touple_of_keys = auth.get_access_token(verifier)
-    list_of_keys = list(touple_of_keys)
-    return list_of_keys
+    def log_in(self):
+        """open the url on browser"""
+        url = self.access.get_authorization_url()
+        webbrowser.open(url)
+        time.sleep(1)
+        self.get_key_and_secret()
 
-def post_tweet(list_of_keys):
-    """post tweet using api"""
-    key = list_of_keys[0]
-    secret = list_of_keys[1]
-    auth.set_access_token(key, secret)
-    api = tweepy.API(auth)
-    username = api.me().name
-    print("Welcome ", username)
-    time.sleep(.5)
-    tweet =input("Tweet here: " ) # toDo 
-    api.update_status(tweet)
+    def get_key_and_secret(self):
+        "ask user to verify the PIN generated in broswer"
+        verifier = input('PIN: ').strip()
+        touple_of_keys = self.access.get_access_token(verifier)
+        list_of_keys = list(touple_of_keys)
+        return list_of_keys
 
-def main():
-        url = get_url()
-        open_browser(url)
-        list_of_keys =  get_key_and_secret()
-        post_tweet(list_of_keys)
-
-if __name__ == "__main__":
-        main()
-
-
+    def post(self, list_of_keys):
+        "post tweet using api"
+        key = list_of_keys[0]
+        secret = list_of_keys[1]
+        self.access.set_access_token(key, secret)
+        api = tweepy.API(self.access)
+        username = api.me().name
+        print("Welcome ", username)
+        time.sleep(.5)
+        tweet = input("Tweet here: ")
+        api.update_status(tweet)
