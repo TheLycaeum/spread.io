40ef4f07ba4811d25d4fad967e42ec8d22d51903
athul <kpathul@outlook.com>
2019-04-01 01:16:44 +0530

    Refactors appWindow

--- appWindow.py
+++ appWindow.py
@@ -1,64 +1,73 @@
-import sys
 import tkinter as tk
 from tkinter import scrolledtext as st
 from tkinter import messagebox as mb
 
-plugins = ["Facebook","Twitter", "Instagram","Google +"]
-n = len(plugins)
-
-
 class Display():
-    """User-interface for the app using Tkinter"""
+    "User-interface for the app using Tkinter"
+
     def __init__(self, window):
-        self.window = window
-        self.window.title("SPREAD.IO")
-        self.window.geometry("500x500")
+        self.win = window
+        self.win.title("Spread.io")
+        self.win.geometry("500x500")
+
         self.show_platforms(plugins)
         self.message_box()
         self.send_button()
         self.show_screen()
 
     def show_screen(self):
-        """Open's the SPREAD.IO window"""
-        self.window.mainloop()
+        "Opens the 'Spread.io' window"
+        self.win.mainloop()
 
     def show_platforms(self, plugins):
-        """Show's the available platforms"""
+        "Shows the available platforms"
         row = 0
         for names in plugins:
             var = tk.IntVar()
-            plat = tk.Checkbutton(self.window, text=names, variable=var, width=40)
-            plat.grid(row=row, column=2)
+            plat = tk.Checkbutton(self.win,
+                                  text=names,
+                                  variable=var,
+                                  width=40)
+            plat.grid(column=2, row=row)
             row += 7
 
     def message_box(self):
-        """to print a label: Message box"""
-        message = tk.Label(self.window, text="Message box", width=40)
+        "Creates a message-box to type the message/content"
+        message = tk.Label(self.win,
+                           text="Message box",
+                           width=40)
         message.grid(column=2, row=90)
         self.scrolled_text()
 
     def scrolled_text(self):
-        """Now let's create scrolled text to type message/content"""
-        self.text_frame = st.ScrolledText(self.window, width=40, height=10)
-        self.text_frame.grid(column=2, row=200)
+        "Creates scrolled text to type message"
+        self.text_frame = st.ScrolledText(self.win,
+                                          width=40,
+                                          height=10)
+        self.text_frame.grid(column=2,
+                             row=200)
 
     def send_button(self):
-        """Button for sending conent"""
-        button = tk.Button(self.window, text="SEND", state='normal', command=self.send_to)
+        "Button for sending content"
+        send_text = self.text_frame.get('1.0', tk.END)
+        button = tk.Button(self.win,
+                           text="SEND",
+                           command=self.send)
         button.grid(row=300, column=2)
-
-    def send_to(self):
-        """send the message/content inside message_box and checks scrolledtext is empty or not"""
+        
+    def send(self):
+        "Sends the content inside message-box"
         send_text = self.text_frame.get('1.0', tk.END)
         if len(send_text) == 1:
-            mb.showinfo("Warning!!!", "Box is empty!")
-        print(send_text)
+            mb.showinfo("Warning", "Box is empty!")
+        # print(send_text)
 
 
+plugins = ["Facebook", "Twitter", "Instagram", "Google +"]
 def main():
-    window= tk.Tk()
+    window = tk.Tk()
     Display(window)
-
+    # app = Display(window)
 
 if __name__ == '__main__':
     main()
