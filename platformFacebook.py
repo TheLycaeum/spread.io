40f1347a412224b22867276da14c31a08854c59b
RohithSml <rohith.sml94@gmail.com>
2019-04-03 19:44:29 +0530

    REFACTOR: Changed to class, Read/Write with ConfigParser

--- platformFacebook.py
+++ platformFacebook.py
@@ -1,90 +1,114 @@
 import webbrowser
-import facebook as f
+from platform import Platform
 from rauth import OAuth2Service
-import rauth
-import requests
+import facebook as fb
+import configparser
 
-def open_browser(url):
-    '''Opens the authorize_url in web-browser'''
-    webbrowser.open(url)
 
-def get_url(params):
-    '''Creates a authorize url for logining in facebook''' 
-    url = facebook.get_authorize_url(**params)
-    return url
+class Facebook(Platform):
+    "Platform for Facebook"
 
-def input_url_with_token():
-    '''Takes the new url which holds the token and splits the string to get the access_token'''
-    url_with_token=''
-    url_with_token = input('After Granting permission please paste url Here:  ')
-    access_tok=url_with_token.split('=')
-    access_to=access_tok[1].split("&")
-    access_token=access_to[0]
-    return access_token
+    def __init__(self, filename):
+        self.configfile = filename
+        self.read_config()
+        self.load_app_apikey()
+        self.check_link()
+        
 
-def get_api(cfg):
-    '''gets the api object from GraphAPI'''
-    page_access_token=None
-    graph=f.GraphAPI(cfg['access_token'])
-    response=graph.get_object('me/accounts')
-    # fetching data attribute of response object
-    for page in response['data']:
-	#finding our page
-        if page['id']==cfg['page_id']:
-            page_access_token=page['access_token']
-            graph=f.GraphAPI(page_access_token)
-    return graph
+    def check_link(self):
+        "Checks whether user is linked to twitter account or not"
+        key = configparser.ConfigParser()
+        key.read(self.configfile)
+        self.access_token=key['Facebook_user']['access_token']
+        try:
+            graph = fb.GraphAPI(self.access_token)
+            response = graph.get_object('me')  
+            self.name = response['name']
+            #print("Logged in as: ", self.name)
+            self.is_linked = True
+        except :
+            self.is_linked = False
+            #raise Exception("Not linked")
 
-def extend_token(cfg, client_id, client_secret):
-    ''' Extend the expiration time of a valid OAuth access token.'''
-    appi=get_api(cfg)
-    extended_token = appi.extend_access_token(client_id, client_secret)
-    return (extended_token['access_token'])
+    def log_in(self):
+        "Open the facebook in browser to authorize the app"
+        params = {'scope': 'publish_pages',
+                  'response_type': 'token',
+                  'redirect_uri': 'https://www.students.thelycaeum.in'}
+        url = self.service.get_authorize_url(**params)
+        webbrowser.open(url)
+        self.get_token_from_url()
 
-def post_message(cfg):
-    '''Post a message in facebook page'''
-    api=get_api(cfg)
-    #print(dir(api))
-    page_id=cfg['page_id']
-    msg='Testing thru Oauth.!!!'
-    
-    #obj_call for posting text
-    api.put_object("me", "feed", message=msg)
-    
-    #obj_call for posting image
-    #api.put_photo(image=open('test.jpg', 'rb'), message='Test for uploading photo next is dp')
-    #obj_call for posting dp
-    #api.put_photo(image=open("jpeg_43.jpg", 'rb'), album_path=page_id + "/picture")  
-    
+    def get_token_from_url(self):
+        "Gets access token from redirect url"
+        url_with_token = input('After Granting permission please paste url Here:  ')
+        self.access_token = url_with_token.split('=')[1].split('&')[0]
+        self.extend_token()
+        
+    def extend_token(self):
+        "Extend the expiration time of a valid OAuth access token."
+        # Not sure whether a return value is required here
+        graph = fb.GraphAPI(self.access_token)
+        self.access_token=graph.extend_access_token(self.client_id, self.secret)
+        #print(self.access_token['access_token'])
+        config = configparser.ConfigParser()
+        config.read(self.configfile)
+        config['Facebook_user']['access_token']= self.access_token['access_token']
+        with open(self.configfile, 'w') as configfile:
+            config.write(configfile)
+                
+    def read_config(self):
+        "Reads the .config file"
+        keys = configparser.ConfigParser()
+        keys.read(self.configfile)        
+        self.page_name=keys['Facebook_user']['page_name']
+        #427579681383478 and name is Test
+        self.app_name = keys['Facebook_client']['name']
+        #'spread'# should be in config
+        self.client_id = keys['Facebook_client']['client_id']
+        #'1234179660062675'                      
+        self.secret = keys['Facebook_client']['client_secret']
+        #'f04ef73cdaf8ecdcbfe536356ef31974'
+        self.access_token= keys['Facebook_user']['access_token']
 
-def main():
-    cfg={
-        'page_id':'427579681383478',
-	'access_token':''
+    def load_app_apikey(self):
+        "Loads app api key"
+        if self.app_name == 'XXXXX' or self.client_id == 'XXXXX' or self.app_name=='XXXXX':
+            raise Exception("You haven't configured the API keys, please read README file")
+        else:
+            self.service = OAuth2Service(name=self.app_name,
+                                         client_id=self.client_id,
+                                         client_secret=self.secret,
+                                         access_token_url='https://graph.facebook.com/oauth/access_token',
+                                         authorize_url='https://graph.facebook.com/oauth/authorize',
+                                         base_url='https://graph.facebook.com/')
 
-        } 
-    client_id = '1234179660062675'
-    client_secret = 'f04ef73cdaf8ecdcbfe536356ef31974'
-    redirect_uri = 'https://www.students.thelycaeum.in'
-    params = {'scope': 'publish_pages',
-              'response_type': 'token',
-              'redirect_uri': redirect_uri}
-    url=get_url(params)
-    open_browser(url)
-    access_token=input_url_with_token()
-    cfg['access_token']=access_token
-    extented_token=extend_token(cfg, client_id, client_secret)
-    post_message(cfg)
+    def post(self,message):
+        " Checks the Page is authentic and then Posts the message in facebook page"
+        # self.graph=fb.GraphAPI(self.access_token)
+        key = configparser.ConfigParser()
+        key.read(self.configfile)
+        self.access_token=key['Facebook_user']['access_token']
+        graph = fb.GraphAPI(self.access_token)
+        response = graph.get_object('me/accounts')
+        #import pdb; pdb.set_trace()
+        #print(response)
+        for page in response['data']:
+	    #finding if the page is granted permission and obtaining its page_token
+            if page['name'] == self.page_name:
+                page_token = page['access_token']
+                self.graph = fb.GraphAPI(page_token)
+        self.graph.put_object("me", "feed", message=message)
+        print("Posted")
 
-    
-    
-if __name__=="__main__":
-    facebook = OAuth2Service(name='spread',
-                             client_id = '1234179660062675',
-                             client_secret = 'f04ef73cdaf8ecdcbfe536356ef31974',
-                             access_token_url='https://graph.facebook.com/oauth/access_token',
-                             authorize_url='https://graph.facebook.com/oauth/authorize',
-                             base_url='https://graph.facebook.com/')
-    
-    
-    main()
+if __name__ == '__main__':
+    pluggin = Facebook('.config')
+    message = 'This works i hope !'
+    if pluggin.is_linked:
+        pluggin.post(message)
+        pass
+    else:
+        pluggin.log_in()
+        pluggin.post(message)
+        
+  
