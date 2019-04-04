94dee377be4eff722599fa42d4177b161331410f
RohithSml <rohith.sml94@gmail.com>
2019-04-04 16:43:41 +0530

    Refactors Facebook  Platform

--- platformFacebook.py
+++ platformFacebook.py
@@ -1,8 +1,8 @@
 import webbrowser
+import configparser
 from platforms import Platform
 from rauth import OAuth2Service
 import facebook as fb
-import configparser
 
 
 class Facebook(Platform):
@@ -13,10 +13,9 @@ class Facebook(Platform):
         self.configfile = filename
         self.load()
         self.check_link()
-        
 
     def check_link(self):
-        "Checks whether user is linked to twitter account or not"
+        "Checks whether user is linked to facebook account or not"
         key = configparser.ConfigParser()
         key.read(self.configfile)
         self.access_token=key['Facebook_user']['access_token']
@@ -24,11 +23,9 @@ class Facebook(Platform):
             graph = fb.GraphAPI(self.access_token)
             response = graph.get_object('me')  
             self.username = response['name']
-            #print("Logged in as: ", self.username)
             self.is_linked = True
         except :
             self.is_linked = False
-            #raise Exception("Not linked")
 
     def log_in(self):
         "Open the facebook in browser to authorize the app"
@@ -38,23 +35,25 @@ class Facebook(Platform):
         url = self.service.get_authorize_url(**params)
         webbrowser.open(url)
 
-    def get_token_from_url(self):
-        "Gets access token from redirect url"
-        url_with_token = input('After Granting permission please paste url Here:  ')
-        self.access_token = url_with_token.split('=')[1].split('&')[0]
+    def write_user_keys(self, url):
+        self.get_token_from_url(url)
         self.extend_token()
+
+        config = configparser.ConfigParser()
+        config.read(self.configfile)
+        config['Facebook_user']['access_token'] = self.access_token['access_token']
+        with open(self.configfile, 'w') as configfile:
+            config.write(configfile)
+
+    def get_token_from_url(self, url):
+        "Gets access token from redirect url"
+        self.access_token = url.split('=')[1].split('&')[0]
         
     def extend_token(self):
         "Extend the expiration time of a valid OAuth access token."
-        # Not sure whether a return value is required here
         graph = fb.GraphAPI(self.access_token)
-        self.access_token=graph.extend_access_token(self.client_id, self.secret)
-        #print(self.access_token['access_token'])
-        config = configparser.ConfigParser()
-        config.read(self.configfile)
-        config['Facebook_user']['access_token']= self.access_token['access_token']
-        with open(self.configfile, 'w') as configfile:
-            config.write(configfile)
+        self.access_token = graph.extend_access_token(self.client_id,
+                                                      self.secret)
 
     def load(self):
         "Loads keys and api"
@@ -66,13 +65,9 @@ class Facebook(Platform):
         keys = configparser.ConfigParser()
         keys.read(self.configfile)        
         self.page_name=keys['Facebook_user']['page_name']
-        #427579681383478 and name is Test
         self.app_name = keys['Facebook_client']['name']
-        #'spread'# should be in config
         self.client_id = keys['Facebook_client']['client_id']
-        #'1234179660062675'                      
         self.secret = keys['Facebook_client']['client_secret']
-        #'f04ef73cdaf8ecdcbfe536356ef31974'
         self.access_token= keys['Facebook_user']['access_token']
 
     def load_app_apikey(self):
@@ -95,8 +90,6 @@ class Facebook(Platform):
         self.access_token=key['Facebook_user']['access_token']
         graph = fb.GraphAPI(self.access_token)
         response = graph.get_object('me/accounts')
-        #import pdb; pdb.set_trace()
-        #print(response)
         for page in response['data']:
 	    #finding if the page is granted permission and obtaining its page_token
             if page['name'] == self.page_name:
@@ -114,5 +107,5 @@ if __name__ == '__main__':
     else:
         pluggin.log_in()
         pluggin.post(message)
-        
+
   
