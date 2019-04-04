d34183873b7982d6b282d48e2dbd9127682b3b86
RohithSml <rohith.sml94@gmail.com>
2019-04-04 12:00:05 +0530

    Added function for check_link(), log_in() and post()

--- spread.py
+++ spread.py
@@ -13,8 +13,8 @@ class Spread():
     def get_plugins(self):
         "Plugin all platforms"
         self.plugins = []
-        self.plugins.append(Twitter(config_file))
-        self.plugins.append(Facebook(config_file))
+        self.plugins.append(Twitter('.config'))  #for testing changed Twitter(config_file) to Twitter('.config')
+        self.plugins.append(Facebook('.config')) #for testing changed Facebook(config_file) to Facebook('.config')
 
     def plugin_names(self):
         "Gets name of all plugged-in platforms"
@@ -22,15 +22,35 @@ class Spread():
         for plug in self.plugins:
             names.append(plug.name)
         return names
+     def check_linked(self, plugin):
+        "Checks if plugin is linked"
+        #the plugin arg should be Twitter/Facebook(config_file) 
+        status=False
+        plugin.check_link()
+        status=plugin.is_linked
+        return status
 
+    def log_in(self, plugin):
+        "Logs in the respective plugin to retrive access_token"
+        plugin.log_in()
 
-
+    def post(self, plugin,message):
+        "Post the message to the respective plugin"
+        plugin.post(message)
+  
 def main():
-    app = Spread()
+    app=Spread()
+    app.get_plugins()
+    #print(app.plugins)
+    #import pdb; pdb.set_trace()
+    #The below is just for testing and shows how to call funtions
+    # twitter_status=app.check_linked(app.plugins[0])
+    # facebook_status=app.check_linked(app.plugins[1])
+    # print(twitter_status, '\n', facebook_status)
 
     names = app.plugin_names()
     appwin = Display(names)
     # print(names)
-
+    
 if __name__ == '__main__':
     main()
