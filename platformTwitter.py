9289e5100e7659ccdd9864634268a72a996066a8
athul <kpathul@outlook.com>
2019-04-04 15:08:21 +0530

    Removes get_user_keys()

--- platformTwitter.py
+++ platformTwitter.py
@@ -1,7 +1,7 @@
 import webbrowser
+import configparser
 import tweepy
 from platforms import Platform
-import configparser
 
 
 class Twitter(Platform):
@@ -11,13 +11,13 @@ class Twitter(Platform):
         self.filename = filename
         self.load()
         self.check_link()
-        
+
     def check_link(self):
         "Check whether user is linked to twitter account or not"
         try:
             self.username = self.api.me().name
             self.is_linked = True
-        except :
+        except:
             self.is_linked = False
             # raise Exception("Error occured")
 
@@ -38,51 +38,33 @@ class Twitter(Platform):
 
     def load_user_key(self):
         "Loads user access token"
-        self.access.set_access_token(self.access_token,self.access_secret)
-
+        self.access.set_access_token(self.access_token,
+                                     self.access_secret)
 
     def read_config(self):
         "Reads config file and store apikey values"
-        keys=configparser.ConfigParser()
-        keys.read(self.filename)
-        self.consumer_key=keys['twitter_app']['consumer_key']
-        self.consumer_secret=keys['twitter_app']['consumer_secret']
-        self.access_token=keys['twitter_user']['access_token']
-        self.access_secret=keys['twitter_user']['access_secret']
-
+        file = configparser.ConfigParser()
+        file.read(self.filename)
+        self.consumer_key = file['twitter_app']['consumer_key']       ## Should remove from instance
+        self.consumer_secret = file['twitter_app']['consumer_secret'] ##
+        self.access_token = file['twitter_user']['access_token']
+        self.access_secret = file['twitter_user']['access_secret']
 
     def log_in(self):
         "Open the twitter in browser to authorize the app"
         url = self.access.get_authorization_url()
         webbrowser.open(url)
 
-    def get_user_keys(self):
-        "Ask user to verify the PIN generated in browser"
-        verifier = input('PIN: ').strip() ###
-        self.keys = self.access.get_access_token(verifier)
-
-    def write_user_keys(self):
-        string = configparser.ConfigParser()
-        string.read(self.filename)
-        print()
-        string['twitter_user']['access_token']=self.keys[0]
-        string['twitter_user']['access_secret']=self.keys[1]
+    def write_user_keys(self, pin):
+        "Get access tokens using the PIN generated in browser"
+        keys = self.access.get_access_token(pin)
+        file = configparser.ConfigParser()
+        file.read(self.filename)
+        file['twitter_user']['access_token'] = keys[0]
+        file['twitter_user']['access_secret'] = keys[1]
         with open(self.filename, 'w') as files:
-            string.write(files)
-            
+            file.write(files)
+
     def post(self, tweet):
         "Posts the tweet using api"
         self.api.update_status(tweet)
-
-
-def main():
-    pluggin = Twitter(".config")
-    while not pluggin.is_linked:
-        pluggin.log_in()
-    print(pluggin.is_linked)
-    message = "Testing; app testing"
-    status = pluggin.post(message)
-    print(status)
-
-if __name__ == '__main__':
-    main()
