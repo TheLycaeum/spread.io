2f242aa71f355e5be2e7a2bb561011974921ef96
fairoos <fairoosnm123@gmail.com>
2019-04-05 16:05:46 +0530

    GREEN: Adds test for read_config function

--- test_platformTwitter.py
+++ test_platformTwitter.py
@@ -14,4 +14,11 @@ def test_initial_value():
 def test_no_value():
     with pytest.raises(Exception):
         obj = Twitter()
+        
+def test_read_config():
+    keys.read(file_name)
+    assert twet.consumer_key == 'Y22pLniUdwNsg5tg7B7UgFlnn'
+    assert twet.consumer_secret == 'P85zJuLD7jBP1eW9EBZMjDbQkdVacljBni5y9gwd0qE5c13bWj'
+    assert twet.access_token == '1112014959901175808-u77cU9I5dbT3NAhs5EboFKnxlSrtBf'
+    assert twet.access_secret == 'HVsJWNoz0ozEZ9GOhypjpbX4N1URITl4dPNq4U9DgzWMz' 
 
