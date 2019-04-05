46112df5c28d6603bc431869cebe0c81566630fd
RohithSml <rohith.sml94@gmail.com>
2019-04-05 16:55:52 +0530

    Test of read_config() for Facebook

--- test_platformFacebook.py
+++ test_platformFacebook.py
@@ -11,3 +11,10 @@ def test_init_value():
     assert face.name == "Facebook"
     assert face.configfile == ".test_config"
 
+def test_read_config():
+     face = Facebook(file_name)
+     assert face.page_name  == 'Test'
+     assert face.client_id  == '1234179660062675'
+     assert face.secret =='f04ef73cdaf8ecdcbfe536356ef31974'
+
+    
