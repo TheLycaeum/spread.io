004a0a0e1984850096a58f44ef0a7bad95628c51
athul <kpathul@outlook.com>
2019-04-07 09:02:09 +0530

    Removes test for read_config()

--- test_platformTwitter.py
+++ test_platformTwitter.py
@@ -1,11 +1,12 @@
- # test_platformTwitter.py
+'''tests for platformTwitter'''
 
 import pytest
 from platformTwitter import Twitter
 import configparser
+
 file_name = ".test_config"
 twet = Twitter(file_name)
-keys=configparser.ConfigParser()
+keys = configparser.ConfigParser()
 
 def test_initial_value():
     assert twet.name ==  "Twitter"
@@ -15,11 +16,6 @@ def test_no_value():
     with pytest.raises(Exception):
         obj = Twitter()
         
-def test_read_config():
-    keys.read(file_name)
-    assert twet.access_token == '1112014959901175808-u77cU9I5dbT3NAhs5EboFKnxlSrtBf'
-    assert twet.access_secret == 'HVsJWNoz0ozEZ9GOhypjpbX4N1URITl4dPNq4U9DgzWMz'
-    assert twet.read_config() == ('Y22pLniUdwNsg5tg7B7UgFlnn', 'P85zJuLD7jBP1eW9EBZMjDbQkdVacljBni5y9gwd0qE5c13bWj')
     
 @pytest.mark.skip(reason="let's fix it later")
 def test_load_app_apikey():
