1e3960e78cf22187db335d7d02c0bcf3dcc6b2c4
athul <kpathul@outlook.com>
2019-04-07 14:38:25 +0530

    Refactors: Facebook
    Adds authenticate_page()
    Adds load_user_keys()
    Removes read_config()
    Removes unnecessary instances

--- platformFacebook.py
+++ platformFacebook.py
@@ -1,8 +1,8 @@
 import webbrowser
 import configparser
-from platforms import Platform
 from rauth import OAuth2Service
 import facebook as fb
+from platforms import Platform
 
 
 class Facebook(Platform):
@@ -11,21 +11,50 @@ class Facebook(Platform):
     def __init__(self, filename):
         self.name = "Facebook"
         self.configfile = filename
-        self.load()
+        self.load() # Need to remove
+
+    def load(self):
+        "Loads keys and api"
+        self.load_app_apikey()
+        access_token = self.load_user_key()
+        self.graph = fb.GraphAPI(access_token)
         self.check_link()
+
+    def load_app_apikey(self):
+        "Loads app api key"
+        keys = configparser.ConfigParser()
+        keys.read(self.configfile)
+        app_name = keys['Facebook_client']['name']
+        client_id = keys['Facebook_client']['client_id']
+        client_secret = keys['Facebook_client']['client_secret']
+
+        #? Do we need to check page_name here?
+        if app_name == 'XXXXX' or client_id == 'XXXXX' or client_secret == 'XXXXX':
+            raise Exception("You haven't configured the API keys. Please read README file.")
+        else:
+            self.service = OAuth2Service(name=app_name,
+                                         client_id=client_id,
+                                         client_secret=client_secret,
+                                         access_token_url='https://graph.facebook.com/oauth/access_token',
+                                         authorize_url='https://graph.facebook.com/oauth/authorize',
+                                         base_url='https://graph.facebook.com/')
+
+    def load_user_key(self):
+        "Loads user access token"
+        keys = configparser.ConfigParser()
+        keys.read(self.configfile)
+        access_token = keys['Facebook_user']['access_token']
+        return access_token
 
     def check_link(self):
         "Checks whether user is linked to facebook account or not"
-        key = configparser.ConfigParser()
-        key.read(self.configfile)
-        self.access_token=key['Facebook_user']['access_token']
         try:
-            graph = fb.GraphAPI(self.access_token)
-            response = graph.get_object('me')  
-            self.username = response['name']
+            response = self.graph.get_object('me')
+            username = response['name']
             self.is_linked = True
-        except :
+        except:
             self.is_linked = False
+
 
     def log_in(self):
         "Open the facebook in browser to authorize the app"
@@ -37,14 +66,31 @@ class Facebook(Platform):
 
     def write_user_keys(self, url):
         "Writes the access_token to .config file"
-        self.get_token_from_url(url)
-        self.extend_token()
+        access_token = self.get_token_from_url(url)
+        exd_token = self.extend_token(access_token)
 
         config = configparser.ConfigParser()
         config.read(self.configfile)
-        config['Facebook_user']['access_token'] = self.access_token['access_token']
+        config['Facebook_user']['access_token'] = exd_token['access_token']
         with open(self.configfile, 'w') as configfile:
             config.write(configfile)
+
+    def get_token_from_url(self, url):
+        "Gets access token from redirect url"
+        splitter = url.split('=')[1].split('&')[0]
+        return splitter
+
+    def extend_token(self, access_token):
+        "Extend the expiration time of a valid OAuth access token."
+        keys = configparser.ConfigParser()
+        keys.read(self.configfile)
+        client_id = keys['Facebook_client']['client_id']
+        client_secret = keys['Facebook_client']['client_secret']
+
+        graph = fb.GraphAPI(access_token)
+        exd_token = graph.extend_access_token(client_id,
+                                              client_secret)
+        return exd_token
 
     def delink(self):
         "Delinks the platform"
@@ -53,79 +99,32 @@ class Facebook(Platform):
         config['Facebook_user']['access_token'] = 'XXXXX'
         with open(self.configfile, 'w') as configfile:
             config.write(configfile)
+        self.is_linked = False
 
 
-    def get_token_from_url(self, url):
-        "Gets access token from redirect url"
-        self.access_token = url.split('=')[1].split('&')[0]
-        
-    def extend_token(self):
-        "Extend the expiration time of a valid OAuth access token."
-        graph = fb.GraphAPI(self.access_token)
-        self.access_token = graph.extend_access_token(self.client_id,
-                                                      self.secret)
+    def authenticate_page(self):
+        "Checks the Page is authentic"
+        response = self.graph.get_object('me/accounts')
 
-    def load(self):
-        "Loads keys and api"
-        self.read_config()
-        self.load_app_apikey()
-                
-    def read_config(self):
-        "Reads the .config file"
         keys = configparser.ConfigParser()
-        keys.read(self.configfile)        
-        self.page_name=keys['Facebook_user']['page_name']
-        self.app_name = keys['Facebook_client']['name']
-        self.client_id = keys['Facebook_client']['client_id']
-        self.secret = keys['Facebook_client']['client_secret']
-        self.access_token= keys['Facebook_user']['access_token']
+        keys.read(self.configfile)
+        page_name = keys['Facebook_user']['page_name']
 
-    def load_app_apikey(self):
-        "Loads app api key"
-        if self.app_name == 'XXXXX' or self.client_id == 'XXXXX' or self.app_name=='XXXXX':
-            raise Exception("You haven't configured the API keys, please read README file")
-        else:
-            self.service = OAuth2Service(name=self.app_name,
-                                         client_id=self.client_id,
-                                         client_secret=self.secret,
-                                         access_token_url='https://graph.facebook.com/oauth/access_token',
-                                         authorize_url='https://graph.facebook.com/oauth/authorize',
-                                         base_url='https://graph.facebook.com/')
-
-    def post(self,message):
-        " Checks the Page is authentic and then Posts the message in facebook page"
-        # self.graph=fb.GraphAPI(self.access_token)
-        self.post_status= False
-        key = configparser.ConfigParser()
-        key.read(self.configfile)
-        self.access_token=key['Facebook_user']['access_token']
-        graph = fb.GraphAPI(self.access_token)
-        try:
-            response = graph.get_object('me/accounts')
-            self.post_status = True
-        except:
-            self.post_status = False
-           # raise Exception("Was Unable to post, check network connection")
-        response = graph.get_object('me/accounts')
         for page in response['data']:
-	    #finding if the page is granted permission and obtaining its page_token
-            if page['name'] == self.page_name:
-                self.page_status= True            
+	#finding if the page is granted permission and obtaining its page_token
+            if page['name'] == page_name:
                 page_token = page['access_token']
                 self.graph = fb.GraphAPI(page_token)
             else:
-                self.page_status= False
                 raise Exception("Cant find page in permission list")
-        self.graph.put_object("me", "feed", message=message)
-            
-if __name__ == '__main__':
-    pluggin = Facebook('.config')
-    message = 'This works i hope !'
-    if pluggin.is_linked:
-        pluggin.post(message)
-        pass
-    else:
-        pluggin.log_in()
-        pluggin.post(message)
 
-  
+    def post(self, message):
+        "Posts the message using api"
+        self.authenticate_page()
+        try:
+            self.graph.put_object("me", "feed", message=message)
+            post_status = True
+        except:
+            post_status = False
+            raise Exception("Was Unable to post, check network connection")
+        return post_status
