b10a632745980959e21e3552e217a4ebabf10f98
kiranuk <Kiran6475@icloud.com>
2019-04-07 17:52:53 +0530

    Edits not required

--- appWindow.py
+++ appWindow.py
@@ -75,7 +75,7 @@ class Display():
             plat = tk.Checkbutton(self.win,
                                   text=plug.name,
                                   variable=var)
-            plat.pack(anchor='w', side=tk.LEFT)
+            plat.pack(anchor='w', side=tk.TOP, padx=10, pady=0)
             
             d_button = self.delink_button(plug)
             var.trace("w", lambda *args:self.callback())
@@ -94,7 +94,7 @@ class Display():
         delink_btn = tk.Button(self.win,
                                text="LOG OUT",
                                command=lambda:[plug.delink(), self.update_platforms()])
-        delink_btn.pack(anchor='e', side=tk.LEFT)
+        delink_btn.pack(anchor='w',side=tk.RIGHT)
         return delink_btn
 
     def update_platforms(self):
