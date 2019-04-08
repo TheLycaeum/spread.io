f556ceaa86204f961615d8a8baf508642592afc8
khaleefakt <ktkhaleefakt@gmail.com>
2019-04-08 16:37:27 +0530

    Added test for check_link fail

--- test_platformTwitter.py
+++ test_platformTwitter.py
@@ -25,6 +25,24 @@ def test_check_link():
     twet.check_link()
     assert twet.is_linked == False
 
+def test_check_link_fail():
+    twet = Twitter(file_name)
+    
+    config = configparser.ConfigParser()
+    config.read(file_name)
+    config['twitter_app']['consumer_key'] = 'Y22pLniUdwNsg5tg7B7UgFlnn'
+    config['twitter_app']['consumer_secret'] = 'P85zJuLD7jBP1eW9EBZMjDbQkdVacljBni5y9gwd0qE5c13bWj'
+    with open (file_name, 'w') as configfile:
+        config.write(configfile)
+    twet.load()
+    twet.check_link()
+    assert twet.is_linked == True
+    reset = configparser.ConfigParser()
+    reset.read(file_name)
+    reset['twitter_app']['consumer_key'] = 'XXXXX'
+    reset['twitter_app']['consumer_secret'] = 'XXXXX'
+    with open (file_name,'w') as config_file:
+        reset.write(config_file)
     
     
 
